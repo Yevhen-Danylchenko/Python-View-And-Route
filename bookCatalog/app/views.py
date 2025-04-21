c395504580801b53094c65b906bbf7d523c8d848
Yevhen-Danylchenko <103068879+Yevhen-Danylchenko@users.noreply.github.com>
2025-04-21 07:33:16 +0300

    Home Work 1.2

--- bookCatalog/app/views.py
+++ bookCatalog/app/views.py
@@ -3,11 +3,41 @@ from django.http import HttpResponse, JsonResponse
 
 
 Books = {
-    1:{"title":"Окрема реальність", "author":"Карлос Кастанеда", "price":200, "genre":"Magic"},
-    2:{"title":"Скажи мені", "author":"Енн Фрейзер", "price":350, "genre":"Fantastic"},
-    3:{"title":"Багаття долі", "author":"Оділь Буе", "price":450, "genre":"Fantastic"},
-    4:{"title":"Нічна зміна", "author":"М. Л. Ріо", "price":270, "genre":"Fantastic"},
-    5:{"title":"ChatGPT", "author":"Олександр Краковецький", "price":300, "genre":"Science"},
+    1:{
+        "title":"Окрема реальність",
+        "author":"Карлос Кастанеда",
+        "price":200,
+        "genre":"Magic",
+        "link":"https://bookchef.ua/upload/resize_cache/iblock/daa/390_390_1/daa72f5804777e323706d9d4e1c7b07f.jpg"
+    },
+    2:{
+        "title":"Скажи мені",
+        "author":"Енн Фрейзер",
+        "price":350,
+        "genre":"Fantastic",
+        "link":"https://bookchef.ua/upload/resize_cache/iblock/421/245_390_1/421ec853aa4f5030628724fda0e5337b.jpg"
+    },
+    3:{
+        "title":"Багаття долі",
+        "author":"Оділь Буе",
+        "price":450,
+        "genre":"Fantastic",
+        "link":"https://bookchef.ua/upload/resize_cache/iblock/75d/390_390_1/75d5ce78abbbe841bc7871fb1ae8a113.png"
+    },
+    4:{
+        "title":"Нічна зміна",
+        "author":"М. Л. Ріо",
+        "price":270,
+        "genre":"Fantastic",
+        "link":"https://bookchef.ua/upload/resize_cache/iblock/31d/390_390_1/31df656500113ca2eb28ab22d4b85c78.png"
+    },
+    5:{
+        "title":"ChatGPT",
+        "author":"Олександр Краковецький",
+        "price":300,
+        "genre":"Science",
+        "link":"https://bookchef.ua/upload/resize_cache/iblock/b53/390_390_1/b535ed6bd1bf04168048a020df8cf8a5.jpg"
+    },
 }
 
 def book_list(request):
@@ -133,11 +163,16 @@ def get_book_by_id(request, book_id):
     book = Books.get(book_id)
     if book:
         book_details = f"""
-            <h2 style = "text-align: center; margin-top: 100px; margin-bottom: 20px;">Знайдені книги</h2>
-            <h2 style = "text-align: center; margin-top: 20px; margin-bottom: 20px;">{ book["title"] }</h2>
-            <h2 style = "text-align: center; margin-top: 20px; margin-bottom: 20px;">{ book["author"]}</h2>
-            <p style = "text-align: center; margin-top: 20px; margin-bottom: 20px;">Ціна: { book["price"] }</p>
-            <p style = "text-align: center; margin-top: 20px; margin-bottom: 20px;">Опис: { book["genre"] }</p>
+            <h1 style = "text-align: center; margin-top: 100px; margin-bottom: 20px;">Знайдені книги</h1>
+            <ul style = "display: flex; justify-content: center; list-style: none; gap: 100px;">
+                <li>
+                    <img src="{ book["link"] }" width=200 alt="photo">
+                    <h2 style = "text-align: center; margin-top: 20px; margin-bottom: 20px;">{ book["title"] }</h2>
+                    <h2 style = "text-align: center; margin-top: 20px; margin-bottom: 20px;">{ book["author"]}</h2>
+                    <p style = "text-align: center; margin-top: 20px; margin-bottom: 20px;">Ціна: { book["price"] }</p>
+                    <p style = "text-align: center; margin-top: 20px; margin-bottom: 20px;">Опис: { book["genre"] }</p>
+                </li>
+            </ul>
         """
         return HttpResponse(book_details)
     else:
@@ -149,11 +184,12 @@ def search_book(request, part_of_title):
         if part_of_title.lower() in book["title"].lower()
     ]
     if result:
-        book_details = """<h2 style = "text-align: center; margin-top: 100px; margin-bottom: 20px;">Знайдені книги</h2>
-            <ul>"""
+        book_details = """<h1 style = "text-align: center; margin-top: 100px; margin-bottom: 20px;">Знайдені книги</h1>
+            <ul style = "display: flex; justify-content: center; list-style: none; gap: 100px;">"""
         for book in result:
             book_details += f"""
-                    <li style = "display: flex; justify-content: center; list-style: none; gap: 100px;">
+                    <li>
+                        <img src="{ book["link"] }" width=200 alt="photo">
                         <h2>{book["title"]}</h2>
                         <h2>{book["author"]}</h2>
                         <p>Ціна: {book["price"]}</p>
@@ -171,18 +207,21 @@ def filter_book_by_price(request, min_price, max_price):
         if min_price <= book["price"] <= max_price
     ]
     if result:
-        book_details = """<h2 style = "text-align: center; margin-top: 100px; margin-bottom: 20px;">Знайдені книги</h2>
-            <ul>"""
+        book_details = """<h1 style = "text-align: center; margin-top: 100px; margin-bottom: 20px;">Знайдені книги</h1>
+            """
         for book in result:
             book_details += f"""
-                            <li style = "display: flex; justify-content: center; list-style: none; gap: 100px;">
-                                <h2>{book["title"]}</h2>
-                                <h2>{book["author"]}</h2>
-                                <p>Ціна: {book["price"]}</p>
-                                <p>Опис: {book["genre"]}</p>
-                            </li>
+                            <ul style = "display: flex; justify-content: center; list-style: none; gap: 100px;">
+                                <li>
+                                    <img src="{ book["link"] }" width=200 alt="photo">
+                                    <h2>{book["title"]}</h2>
+                                    <h2>{book["author"]}</h2>
+                                    <p>Ціна: {book["price"]}</p>
+                                    <p>Опис: {book["genre"]}</p>
+                                </li>
+                            </ul>
                         """
-            book_details += "</ul>"
+
         return HttpResponse(book_details)
     else:
         return HttpResponse("<h2>Книга не знайдена</h2>", status=404)
